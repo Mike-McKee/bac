00b3e4c5883c67f75b358bd87023a939d1994893
MIchael McKee <mike@dorkydata.com>
2023-06-23 20:13:48 -0400

    folder for python code

---
+++ python_code/pycode.py
@@ -0,0 +1,18 @@
+"""
+Here's the plan. Write a script that has the user input their weight
+and the number of drinks they had during the night. 
+
+Then we'll use their inputs to find the corresponding row in the
+raw_bac_data.csv and print two things:
+1) their bac
+2) whether or not they can drive
+"""
+
+
+
+
+
+
+
+
+
