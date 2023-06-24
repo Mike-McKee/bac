978f58c393b976b500c892c13c19737682f0473a
MIchael McKee <mike@dorkydata.com>
2023-06-24 11:19:51 -0400

    fixed comments

--- python_code/pycode_1.py
+++ python_code/pycode_1.py
@@ -1,11 +1,6 @@
 """
-Here's the plan. Write a script that has the user input their weight
-and the number of drinks they had during the night. 
-
-Then we'll use their inputs to find the corresponding row in the
-raw_bac_data.csv and print two things:
-1) their bac
-2) whether or not they can drive
+This script uses a long while statement that ensures the inputs fit the values
+in the csv file.
 """
 
 import pandas as pd
